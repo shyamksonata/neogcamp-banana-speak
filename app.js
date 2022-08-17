19f165ec949de3a38b7120ad582abd1e083f815c
shyamksonata <shyamk.sonata@gmail.com>
2022-08-17 12:32:12 +0530

    banana speak final

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-// console.log("script is working from different file.")
-// var username = prompt("Give me username")
-// alert("wow! the script works" + username)
-var btntranslate = document.querySelector("#btn-translate")
-var txtInput= document.querySelector("#txt-input")
-console.log(txtInput)
-btntranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked")
-    console.log("input", txtInput.value)
-})
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput =document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
+//console.log(outputDiv)
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+function errorHandler(error){
+    console.log("error occoured", error)
+    alert("something went wrong")
+}
+
+function clickhandler(){
+    //outputDiv.innerText="shyam kumar" +txtInput.value
+    var inputText = txtInput.value 
+    fetch(getTranslationURL(inputText))
+    .then(respone => respone.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        
+    })
+    .catch(errorHandler)
+    //console.log("Clicked")
+    //console.log("input", txtInput.value)
+}
+btnTranslate.addEventListener("click", clickhandler)
